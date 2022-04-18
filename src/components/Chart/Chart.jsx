1ff46149340b6e464a86dbb3b110c00b42447adc
aanand93 <77586218+aanand93@users.noreply.github.com>
2022-04-18 19:20:54 -0400

    Upadted to fix bug, still didn't fix the bug. Api has not been updated

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -18,63 +18,58 @@ const Chart = ({ data: {confirmed, deaths, recovered}, country }) => {
 		fetchAPI();
 	}, []);
 
+	
+	const barChart = (
+		confirmed ? (
+			<Bar
+			data={{
+				labels: ['Infected', 'Recovered', 'Deaths'],
+				datasets: [
+					{
+						label: 'People',
+              backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+              data: [confirmed.value, recovered.value, deaths.value],
+            },
+		],
+	}}
+	options={{
+		legend: { display: false },
+		title: { display: true, text: `Current state in ${country}` },
+	}}
+	/>
+    ) : null
+	);
+	
 	const lineChart = (
 		dailyData.length
 			? (
 			<Line
-        data={{
-          labels: dailyData.map(({ date }) => new Date(date).toLocaleDateString()),
-          datasets: [{
-            data: dailyData.map((data) => data.confirmed),
-            label: 'Infected',
-            borderColor: '#3333ff',
-            fill: true,
-          }, {
-            data: dailyData.map((data) => data.deaths),
-            label: 'Deaths',
-            borderColor: 'red',
-            backgroundColor: 'rgba(255, 0, 0, 0.5)',
-            fill: true,
-          },  {
-            data: dailyData.map((data) => data.recovered),
-            label: 'Recovered',
-            borderColor: 'green',
-            backgroundColor: 'rgba(0, 255, 0, 0.5)',
-            fill: true,
-          },
-          ],
-        }}
-      />
-    ) : null
+		data={{
+		  labels: dailyData.map(({ date }) => new Date(date).toLocaleDateString()),
+		  datasets: [{
+			data: dailyData.map((data) => data.confirmed),
+			label: 'Infected',
+			borderColor: '#3333ff',
+			fill: true,
+		  }, {
+			data: dailyData.map((data) => data.deaths),
+			label: 'Deaths',
+			borderColor: 'red',
+			backgroundColor: 'rgba(255, 0, 0, 0.5)',
+			fill: true,
+		  },  {
+			data: dailyData.map((data) => data.recovered),
+			label: 'Recovered',
+			borderColor: 'green',
+			backgroundColor: 'rgba(0, 255, 0, 0.5)',
+			fill: true,
+		  },
+		  ],
+		}}
+	  />
+	) : null
   );
-
-		console.log(confirmed, recovered, deaths)
-
-	const barChart = (
-		confirmed
-		? (
-			<Bar
-				data={{
-					lables: ['Infected', 'Recovered', 'Deaths'],
-					datasets: [{
-						label: 'People',
-						backgroundColor: [
-							'rgba(0, 0, 255, 0.5)',
-							'rgba(0, 255, 0, 0.5)',
-							'rgba(255, 0, 0, 0.5)',
-
-						],
-						data: [confirmed.value, recovered.value, deaths.value],
-					}]
-				}}
-				options={{
-					legend: { display: false},
-					title: { display: true, text:`Current state in ${country}`}
-				}}
-			/>
-		) : null
-	);
-
+	
 	return ( 
 		<div className={styles.container}>
 			{country ? barChart : lineChart}
